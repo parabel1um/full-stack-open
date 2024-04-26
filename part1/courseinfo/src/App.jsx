c21b2a3faa1b163bb1443ab3b63b82094b1983df
parabel1um <167568865+parabel1um@users.noreply.github.com>
2024-04-26 21:27:17 +0300

    1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Header = (props) => {
+  console.log(props);
   return (
     <>
       <h1>{props.name}</h1>
@@ -19,11 +20,12 @@ const Part = (props) => {
 };
 
 const Content = (props) => {
+  console.log(props.part1);
   return (
     <div>
-      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+      <Part name={props.part1.name} exercises={props.part1.exercises} />
+      <Part name={props.part2.name} exercises={props.part2.exercises} />
+      <Part name={props.part3.name} exercises={props.part3.exercises} />
     </div>
   );
 };
@@ -31,29 +33,31 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <>
-      <p>
-        Number of exercises{" "}
-        {props.exercises[0].exercises +
-          props.exercises[1].exercises +
-          props.exercises[2].exercises}
-      </p>
+      <p>Number of exercises {props.exercises}</p>
     </>
   );
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const parts = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header name={course} />
-      <Content parts={parts} />
-      <Total exercises={parts} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total exercises={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
