102718bba7fed61843592e485ec2f6525285c5cc
parabel1um <167568865+parabel1um@users.noreply.github.com>
2024-04-27 21:35:39 +0300

    1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+
+const Display = (props) => <p>{props.value}</p>;
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const handleBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <h1>statistics</h1>
+      <Display value={"good " + good} />
+      <Display value={"neutral " + neutral} />
+      <Display value={"bad " + bad} />
+    </div>
+  );
+};
+
+export default App;
