a914d2fcc1d3c410534cf526ab73711e6edb42a3
parabel1um <167568865+parabel1um@users.noreply.github.com>
2024-04-30 18:02:02 +0300

    2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,45 +1,5 @@
 import React from "react";
-
-const Total = (props) => {
-  return <b>total of {props.total} exercises</b>;
-};
-
-const Header = (props) => {
-  return <h1>{props.header}</h1>;
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  );
-};
-
-const Content = (props) => {
-  return (
-    <div>
-      {props.parts.map((part) => (
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Course = (props) => {
-  const total = props.course.parts.reduce(
-    (sum, part) => sum + part.exercises,
-    0
-  );
-
-  return (
-    <div>
-      <Header header={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total total={total} />
-    </div>
-  );
-};
+import Course from "./components/course";
 
 const App = () => {
   const courses = [
