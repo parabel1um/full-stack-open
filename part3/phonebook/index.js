d8129c2ddde887c40238648e53e83781f99e6717
parabel1um <167568865+parabel1um@users.noreply.github.com>
2024-05-18 21:26:47 +0300

    3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 let numbers = [
@@ -25,6 +26,7 @@ let numbers = [
 ];
 
 app.use(express.json());
+app.use(morgan("tiny"));
 
 app.get("/api/persons", (request, response) => {
   response.json(numbers);
