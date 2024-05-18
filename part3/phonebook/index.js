41f92181f54756c71dccabc6104ac1c5018677fd
parabel1um <167568865+parabel1um@users.noreply.github.com>
2024-05-18 20:12:30 +0300

    3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -47,6 +47,12 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  numbers = numbers.filter((person) => person.id !== Number(request.params.id));
+
+  response.status(204).end();
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
